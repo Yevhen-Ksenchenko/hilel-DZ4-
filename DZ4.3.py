a0ac3e4805eaf73bcfc5e635e7715bb9a9a3d55b
Yevhen Ksenchenko <harlampiy2@gmail.com>
2025-06-13 06:10:44 +0300

    modified DZ4.3.py

--- DZ4.3.py
+++ DZ4.3.py
@@ -1,14 +1,6 @@
-# ось так теж можна)) всі умови виконані)) - ще куча домашок попереду
-elist = input("Enter from 3 to 10 numders: ")
-if not elist.isdigit():
-    print("Only numbers")
-elif len(elist) < 3:
-    print("more")
-elif len(elist) > 10:
-    print("less")
-    elist = str(elist)
-else:
-
-    elist1 = [elist[0], elist[2], elist[-2]]
-    print('[', *elist, ']', sep=" ")
-    print(elist1)
+import random
+value = random.randint(3, 10)
+elist = [random.randint(1, 10) for _ in range(value)]
+elist1 = [elist[0], elist[2], elist[-2]]
+print(elist)
+print(elist1)
